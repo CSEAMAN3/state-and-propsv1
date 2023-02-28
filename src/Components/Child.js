a1a1bde7daf87dd1aaf5c5f47a41b8385f2b4162
Tim Smith <tim.smith2814@gmail.com>
2023-02-28 15:52:26 +0000

    Betterify it all

--- src/Components/Child.js
+++ src/Components/Child.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import "./Child.css"
-import boy from "../Images/boy.jpg"
+import { useState } from "react";
+import "./Child.css";
+import boy from "../Images/boy.jpg";
 
-export default function Child({name, surname}) {
+export default function Child({ name, surname }) {
+  // new state variable
+  //     state variable    mutation function              initial value
+  const [patsOnBillysHead, setPatsOnBillysHead] = useState(0);
+
+  function handlePat() {
+    setPatsOnBillysHead(patsOnBillysHead + 1);
+  }
 
   return (
     <div>
-        <h2>The boys name is {name} {surname}</h2>
-        <img src={boy} alt="Billie smiling" />
+      <h2>
+        The boys name is {name} {surname}
+      </h2>
+      <img src={boy} alt="Billy smiling" onClick={handlePat} />
+      <p>Number of pats on his head 👋: {patsOnBillysHead}</p>
     </div>
-  )
+  );
 }
